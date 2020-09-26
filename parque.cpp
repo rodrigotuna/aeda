549faf3071ae1a0a710dd760ebe19241de4fdc10
rodrigotuna <53995460+rodrigotuna@users.noreply.github.com>
2020-09-26 22:27:07 +0100

    Add files via upload

---
+++ parque.cpp
@@ -0,0 +1,90 @@
+#include "parque.h"
+#include <vector>
+
+using namespace std;
+
+ParqueEstacionamento::ParqueEstacionamento(unsigned int lot, unsigned int nMaxCli) : lotacao(lot), numMaximoClientes(nMaxCli) {
+	vagas = lot;
+}
+
+unsigned ParqueEstacionamento::getNumLugares() const {
+	return lotacao;
+}
+
+unsigned ParqueEstacionamento::getNumMaximoClientes() const {
+	return numMaximoClientes;
+}
+
+int ParqueEstacionamento::posicaoCliente(const string& nome) const {
+	if (find(clientes.begin(), clientes.end(), nome) == clientes.end()) {
+		return -1;
+	}
+
+	return (find(clientes.begin(), clientes.end(), nome) - clientes.end());
+}
+
+bool ParqueEstacionamento::adicionaCliente(const string& nome) {
+	if (clientes.size() == numMaximoClientes) {
+		return false;
+	}
+	InfoCartao cartao(nome,false);
+	clientes.push_back(cartao);
+	return true;
+}
+
+bool ParqueEstacionamento::entrar(const string& nome) {
+	int posicao = posicaoCliente(nome);
+	if (posicao == -1) {
+		return false;
+	}
+
+	if (clientes[posicao].presente) {
+		return false;
+	}
+
+	if (!vagas) {
+		return false;
+	}
+
+	clientes[posicao].presente = true;
+	vagas--;
+	return true;
+}
+
+bool ParqueEstacionamento::retiraCliente(const string& nome) {
+	int posicao = posicaoCliente(nome);
+
+	if (posicao == -1) {
+		return false;
+	}
+
+	if (clientes[posicao].presente) {
+		return false;
+	}
+
+	clientes.erase(clientes.begin() + posicao);
+}
+
+bool ParqueEstacionamento::sair(const string& nome) {
+	int posicao = posicaoCliente(nome);
+
+	if (posicao == -1) {
+		return false;
+	}
+
+	if (!clientes[posicao].presente) {
+		return false;
+	}
+
+	clientes[posicao].presente = false;
+	return true;
+}
+
+unsigned ParqueEstacionamento::getNumLugaresOcupados() const {
+	return lotacao - vagas;
+}
+
+unsigned ParqueEstacionamento::getNumClientesAtuais() const {
+	return clientes.size();
+}
+
