ba215a4038646282bf88d6cc908f98a1b709f537
rodrigotuna <53995460+rodrigotuna@users.noreply.github.com>
2020-09-29 10:54:37 +0100

    Add files via upload

--- parque.cpp
+++ parque.cpp
@@ -1,3 +1,4 @@
+
 #include "parque.h"
 #include <vector>
 #include <algorithm>
@@ -5,89 +6,90 @@
 using namespace std;
 
 ParqueEstacionamento::ParqueEstacionamento(unsigned int lot, unsigned int nMaxCli) : lotacao(lot), numMaximoClientes(nMaxCli) {
-	vagas = lot;
+    vagas = lot;
 }
 
 unsigned ParqueEstacionamento::getNumLugares() const {
-	return lotacao;
+    return lotacao;
 }
 
 unsigned ParqueEstacionamento::getNumMaximoClientes() const {
-	return numMaximoClientes;
+    return numMaximoClientes;
 }
 
 int ParqueEstacionamento::posicaoCliente(const string& nome) const {
-	for (size_t i = 0; i < clientes.size(); i++) {
-		if (clientes[i].nome == nome) {
-			return i;
-		}
-	}
+    for (size_t i = 0; i < clientes.size(); i++) {
+        if (clientes[i].nome == nome) {
+            return i;
+        }
+    }
 
-	return -1;
+    return -1;
 }
 
 bool ParqueEstacionamento::adicionaCliente(const string& nome) {
-	if (clientes.size() == numMaximoClientes) {
-		return false;
-	}
-	InfoCartao cartao(nome,false);
-	clientes.push_back(cartao);
-	return true;
+    if (clientes.size() == numMaximoClientes) {
+        return false;
+    }
+    InfoCartao cartao(nome,false);
+    clientes.push_back(cartao);
+    return true;
 }
 
 bool ParqueEstacionamento::entrar(const string& nome) {
-	int posicao = posicaoCliente(nome);
-	if (posicao == -1) {
-		return false;
-	}
+    int posicao = posicaoCliente(nome);
+    if (posicao == -1) {
+        return false;
+    }
 
-	if (clientes[posicao].presente) {
-		return false;
-	}
+    if (clientes[posicao].presente) {
+        return false;
+    }
 
-	if (!vagas) {
-		return false;
-	}
+    if (!vagas) {
+        return false;
+    }
 
-	clientes[posicao].presente = true;
-	vagas--;
-	return true;
+    clientes[posicao].presente = true;
+    vagas--;
+    return true;
 }
 
 bool ParqueEstacionamento::retiraCliente(const string& nome) {
-	int posicao = posicaoCliente(nome);
+    int posicao = posicaoCliente(nome);
 
-	if (posicao == -1) {
-		return false;
-	}
+    if (posicao == -1) {
+        return false;
+    }
 
-	if (clientes[posicao].presente) {
-		return false;
-	}
+    if (clientes[posicao].presente) {
+        return false;
+    }
 
-	clientes.erase(clientes.begin() + posicao);
+    clientes.erase(clientes.begin() + posicao);
+    return true;
 }
 
 bool ParqueEstacionamento::sair(const string& nome) {
-	int posicao = posicaoCliente(nome);
+    int posicao = posicaoCliente(nome);
 
-	if (posicao == -1) {
-		return false;
-	}
+    if (posicao == -1) {
+        return false;
+    }
 
-	if (!clientes[posicao].presente) {
-		return false;
-	}
+    if (!clientes[posicao].presente) {
+        return false;
+    }
 
-	clientes[posicao].presente = false;
-	return true;
+    clientes[posicao].presente = false;
+    return true;
 }
 
 unsigned ParqueEstacionamento::getNumLugaresOcupados() const {
-	return lotacao - vagas;
+    return lotacao - vagas;
 }
 
 unsigned ParqueEstacionamento::getNumClientesAtuais() const {
-	return clientes.size();
+    return clientes.size();
 }
 
